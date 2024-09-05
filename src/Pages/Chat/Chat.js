47af82973a7ba03b61559caba30e53fbeda817ce
vishnuvardhanjs-alt <vichudsvv@gmail.com>
2024-09-05 10:55:54 +0530

    [Added] Sentiment Score

--- src/Pages/Chat/Chat.js
+++ src/Pages/Chat/Chat.js
@@ -1,111 +1,159 @@
-import React, { useEffect, useState, useRef } from 'react'
-import './Chat.css'
-import { useLocation } from 'react-router-dom'
-import { addDoc, collection, onSnapshot, orderBy, query, Timestamp } from 'firebase/firestore';
-import { db } from '../../firebase/firebase';
-import { MessageBox } from 'react-chat-elements';
-import 'react-chat-elements/dist/main.css';
-import { Filter } from 'bad-words'
-
-
+import React, { useEffect, useState, useRef, useReducer } from "react";
+import "./Chat.css";
+import { useLocation } from "react-router-dom";
+import {
+  addDoc,
+  collection,
+  onSnapshot,
+  orderBy,
+  query,
+  Timestamp,
+} from "firebase/firestore";
+import { db } from "../../firebase/firebase";
+import { MessageBox } from "react-chat-elements";
+import "react-chat-elements/dist/main.css";
+import { Filter } from "bad-words";
+import Sentiment from "sentiment";
 
 function Chat() {
-    const endRef = useRef(null);
-    const filter = new Filter();
+  const endRef = useRef(null);
+  const filter = new Filter();
+  let sentiment = new Sentiment();
 
-    filter.addWords('some', 'bad', 'word')
-    
-    const { state } = useLocation();
-    const { from, to } = state;
+  filter.addWords("some", "bad", "word");
 
-    const [msgs, setMsgs] = useState([])
-    const [text, setText] = useState("")
+  const { state } = useLocation();
+  const { from, to } = state;
 
-    async function addNewDocument() {
-        try {
-            const collection_id = (from + to).split('').sort().join('')
-            const docRef = await addDoc(collection(db, collection_id), {
-                from: from,
-                to: to,
-                message: filter.clean(text),
-                time: Timestamp.now()
-            });
-            console.log("Document written with ID: ", docRef.id);
-        } catch (e) {
-            console.error("Error adding document: ", e);
-        }
+  const [msgs, setMsgs] = useState([]);
+  const [text, setText] = useState("");
+
+  const [senti, setSenti] = useState(0);
+  const [emoji, setEmoji] = useState("😐");
+
+  async function addNewDocument() {
+    try {
+      const collection_id = (from + to).split("").sort().join("");
+      const docRef = await addDoc(collection(db, collection_id), {
+        from: from,
+        to: to,
+        message: text,
+        time: Timestamp.now(),
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
     }
+  }
 
-    useEffect(()=>{
-        endRef.current?.lastElementChild?.scrollIntoView()
-    },[msgs])
-
-    useEffect(() => {
-        const objDiv = document.getElementsByClassName('msg-cont');
-        objDiv[0].scrollTop = objDiv[0].scrollHeight;
-        const collection_id = (from + to).split('').sort().join('')
-        const q = query(collection(db, collection_id), orderBy("time"));
-        console.log("running")
-        const unsubscribe = onSnapshot(q, (snapshot) => {
-            let cur_msgs = []
-            snapshot.docs.map((doc) => {
-                let cur_doc = doc.data()
-                if (cur_doc.from === localStorage.getItem("cur_user_id")) {
-                    cur_msgs.push({ ...cur_doc, position: "right" })
-                } else {
-                    cur_msgs.push({ ...cur_doc, position: "left" })
-                }
-            });
-            setMsgs(cur_msgs);
-        });
-
-        // Cleanup the subscription when the component unmounts
-        return () => unsubscribe();
-    }, []);
-
-    async function handleSend() {
-        if (text !== "") {
-            await addNewDocument()
-            setText("")
-        }
+  useEffect(()=>{
+    if (senti == 69) {
+        setEmoji("🥵")
+    } else if (senti > 0) {
+        setEmoji("😄")
+    } else if (senti < 0) {
+        setEmoji("☹️")
+    } else {
+        setEmoji("😐")
     }
+  },[senti])
 
-    const handleKeyDown = (event) => {
-        if (event.key === 'Enter') {
-            event.preventDefault();
-            handleSend();
+  useEffect(() => {
+    endRef.current?.lastElementChild?.scrollIntoView();
+    let msg_len = msgs.length
+    let score_count = 0
+    msgs.map((val,index)=>{
+        score_count+= sentiment.analyze(val.message).comparative
+    })
+    setSenti( (score_count / msg_len).toFixed(2))
+  }, [msgs]);
+
+  useEffect(() => {
+    const objDiv = document.getElementsByClassName("msg-cont");
+    objDiv[0].scrollTop = objDiv[0].scrollHeight;
+    const collection_id = (from + to).split("").sort().join("");
+    const q = query(collection(db, collection_id), orderBy("time"));
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      let cur_msgs = [];
+      snapshot.docs.map((doc) => {
+        let cur_doc = doc.data();
+        if (cur_doc.from === localStorage.getItem("cur_user_id")) {
+          cur_msgs.push({ ...cur_doc, position: "right" });
+        } else {
+          cur_msgs.push({ ...cur_doc, position: "left" });
         }
-    };
+      });
+      setMsgs(cur_msgs);
+    });
+    return () => unsubscribe();
+  }, []);
 
+  async function handleSend() {
+    if (text !== "") {
+      await addNewDocument();
+      setText("");
+    }
+  }
 
-    return (
-        <div className="main-cont">
-            <div className="msg-view">
-                <div className="msg-cont" ref={endRef}>
-                    {
-                        msgs.map((item, index) => {
-                            return (
-                                <MessageBox
-                                    key={index}
-                                    position={item.position}
-                                    type={'text'}
-                                    text={item.message}
-                                />
-                            )
-                        })
-                    }
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      handleSend();
+    }
+  };
 
-                </div>
-                <div className="messageBox">
-                    <input required="" onKeyDown={handleKeyDown} onChange={(e) => { setText(e.target.value) }} value={text} placeholder="Message..." type="text" id="messageInput" />
-                    <button id="sendButton" type='submit'
-                        onClick={() => { handleSend() }}>
-                        Send
-                    </button>
-                </div>
-            </div>
-        </div >
-    )
+  return (
+    <div className="main-cont">
+      <div className="msg-view">
+        <div className="msg-cont">
+          {msgs.map((item, index) => {
+            return (
+              <MessageBox
+                position={item.position}
+                type={"text"}
+                text={filter.clean(item.message)}
+              />
+            );
+          })}
+        </div>
+        <div className="messageBox">
+          <input
+            required=""
+            onKeyDown={handleKeyDown}
+            onChange={(e) => {
+              setText(e.target.value);
+            }}
+            value={text}
+            placeholder="Message..."
+            type="text"
+            id="messageInput"
+          />
+          <button
+            id="sendButton"
+            type="submit"
+            onClick={() => {
+              handleSend();
+            }}
+          >
+            Send
+          </button>
+        </div>
+      </div>
+      <div className="right-cont">
+        {!isNaN(senti) ? (
+          <div className="score-cont">
+            <p>Sentiment Score : {senti}</p>
+            <p className="emoji">{emoji}</p>
+          </div>
+        ) : (
+          <div className="score-cont">
+            <p>Sentiment Score : 0.00</p>
+            <p className="emoji">😐</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default Chat
+export default Chat;
